6ebb4d2c44065726e6f5ebf2e6cd7066a88bd131
Callas Foundation <admin@callasfoundation.org.za>
2026-07-31 16:25:28 -0500

    Rename GalleryImageDto to CreateGalleryImageDto

--- Callas.API/DTOs/Gallery/GalleryImageDto.cs
+++ Callas.API/DTOs/Gallery/GalleryImageDto.cs
@@ -1,10 +1,20 @@
-﻿// DTOs/Gallery/GalleryImageDto.cs
+// DTOs/Gallery/CreateGalleryImageDto.cs
+using System.ComponentModel.DataAnnotations;
+
 namespace Callas.API.DTOs.Gallery
 {
-    public class GalleryImageDto
+    public class CreateGalleryImageDto
     {
-        public int Id { get; set; }
+        [Required, Url]
         public string ImageUrl { get; set; } = string.Empty;
+
+        [Required, MaxLength(200)]
         public string Caption { get; set; } = string.Empty;
+
+        [MaxLength(100)]
+        public string Category { get; set; } = "Uncategorised";
+
+        [MaxLength(10)]
+        public string MediaType { get; set; } = "image";
     }
 }
