915f40b4f3eec54bf7cd531f07c8cde56ab53e1b
Callas Foundation <admin@callasfoundation.org.za>
2026-07-31 16:37:04 -0500

    Add authorization to gallery image management actions
    Added authorization attributes for image upload, update, and delete actions.

--- Callas.API/Controllers/GalleryController.cs
+++ Callas.API/Controllers/GalleryController.cs
@@ -1,4 +1,4 @@
-﻿using Callas.API.DTOs.Gallery;
+using Callas.API.DTOs.Gallery;
 using Callas.API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
@@ -30,6 +30,7 @@ namespace Callas.API.Controllers
             return image is null ? NotFound() : Ok(image);
         }
 
+        [Authorize(Roles = "Admin")]
         [HttpPost]
         public async Task<IActionResult> UploadImage(CreateGalleryImageDto dto)
         {
@@ -37,6 +38,7 @@ namespace Callas.API.Controllers
             return CreatedAtAction(nameof(GetImage), new { id = created.Id }, created);
         }
 
+        [Authorize(Roles = "Admin")]
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateImage(int id, UpdateGalleryImageDto dto)
         {
@@ -44,6 +46,7 @@ namespace Callas.API.Controllers
             return updated ? NoContent() : NotFound();
         }
 
+        [Authorize(Roles = "Admin")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteImage(int id)
         {
