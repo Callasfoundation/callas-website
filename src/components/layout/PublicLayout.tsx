071c65d4bc6267d038723f063063e5c24958a91f
Callas Foundation <admin@callasfoundation.org.za>
2026-08-11 02:40:48 -0500

    Add logo image to PublicLayout component

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -5,13 +5,20 @@ import { Footer } from "./Footer";
 import { Chatbot } from "./Chatbot";
 import { NewsPopup } from "./NewsPopup";
 import { CookieBanner } from "./CookieBanner";
+import logo from "@/assets/images/logo/callas-logo.png";
 
 export function PublicLayout() {
   const key = useRouterState({ select: (s) => s.location.pathname });
   return (
     <div className="min-h-screen flex flex-col bg-white">
+      <img
+        src={logo}
+        alt=""
+        aria-hidden="true"
+        className="fixed bottom-4 right-4 h-20 w-20 sm:h-28 sm:w-28 opacity-[0.06] pointer-events-none select-none z-0"
+      />
       <Navbar />
-      <main className="flex-1">
+      <main className="flex-1 relative z-10">
         <AnimatePresence mode="wait">
           <motion.div
             key={key}
